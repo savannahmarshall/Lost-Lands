4e7478ca2d53eb83cfadc5ce5b013059e0bcf9be
savannahmarshall <savvymarshall@gmail.com>
2024-09-14 20:22:58 -0700

    installed mongoose, updated server.js and connection.js to connect to mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techmatchup');
+//MongoDB URI for Mongo atlas, use this to test locally: 'mongodb://localhost:27017/your-database')
+const mongoURI = 'mongodb+srv://savvymarshall:duckies43@project3cluster.h5vfp.mongodb.net/?retryWrites=true&w=majority&appName=project3cluster';
 
-module.exports = mongoose.connection;
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;

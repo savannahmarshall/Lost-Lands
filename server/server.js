115663172961ad83e615a2c978f4013bb26dd2e3
savannahmarshall <savvymarshall@gmail.com>
2024-09-16 20:47:36 -0700

    added code for render deploy to server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path'); 
 const { typeDefs, resolvers } = require('./schemas');
 const connectDB = require('./config/connection');
 
@@ -15,21 +16,29 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files (e.g., if you're serving an HTML file)
+app.use(express.static('public'));  
+
 // Apollo Server setup
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => {
-    // Add the JWT_SECRET to the context if needed
     return { JWT_SECRET: process.env.JWT_SECRET || 'your_secret_key' };
   },
 });
 
-// Start Apollo Server
+// Add a route to handle the root
+app.get('/', (req, res) => {
+
+  //static HTML
+  res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
+});
+
+// Start Apollo Server and Express Server
 server.start().then(() => {
   server.applyMiddleware({ app });
 
-  // Start the Express server
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     console.log(`GraphQL endpoint: http://localhost:${PORT}${server.graphqlPath}`);
